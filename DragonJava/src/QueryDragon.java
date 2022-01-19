3b5e665b2d3cc946fca346185430c5559ed968ed
BAUDINO PIERRE <PIE.BAUDINO@pedago.mermoz.edu>
2022-01-19 16:34:01 +0100

    correction update

--- DragonJava/src/QueryDragon.java
+++ DragonJava/src/QueryDragon.java
@@ -8,21 +8,22 @@ public class QueryDragon extends MyConnexion{
 	public static boolean create(String dragon,int id, String sexe, int longueur, int ecailles, String feu,String amour) {
 		boolean flag=false;
 		try {
-			String query ="INSERT INTO dragons2 (Dragon, DragonID, Sexe, Longueur, NombreEcailles, CracheDuFeu?, ComportementAmoureux) VALUES (?,?,?,?,?,?,?)";
+			String query ="INSERT INTO dragons2 (Dragon, DragonID, Sexe, Longueur, NombreEcailles, CracheDuFeu, ComportementAmoureux) VALUES (?,?,?,?,?,?,?)";
 			PreparedStatement declaration= accessDataBase.prepareStatement(query);
 			declaration.setString(1, dragon);
-			declaration.setInt(1, id);
-			declaration.setString(1, sexe);
-			declaration.setInt(1, longueur);
-			declaration.setInt(1, ecailles);
-			declaration.setString(1, feu);
-			declaration.setString(1, amour);
+			declaration.setInt(2, id);
+			declaration.setString(3, sexe);
+			declaration.setInt(4, longueur);
+			declaration.setInt(5, ecailles);
+			declaration.setString(6, feu);
+			declaration.setString(7, amour);
 			int executeUpdate=declaration.executeUpdate();
 			flag=(executeUpdate==1);
 		}catch (Exception e) {
 			// TODO: handle exception
 			System.err.println("Erreur d'insertion dragon"+e.getMessage());
 		}
+		System.out.println("fin requête create");
 		return flag;
 	}
 	public static void readAll() {
@@ -32,16 +33,17 @@ public class QueryDragon extends MyConnexion{
 			ResultSet resultat=declaration.executeQuery(query);
 			while (resultat.next()) {
 				Object[] row =new Object[] {
-						resultat.getInt("DragonID"),
 						resultat.getString("Dragon"),
+						resultat.getInt("DragonID"),
 						resultat.getString("Sexe"),
 						resultat.getString("Longueur"),
 						resultat.getString("NombreEcailles"),
-						resultat.getString("CracheDuFeu?"),
+						resultat.getString("CracheDuFeu"),
 						resultat.getString("ComportementAmoureux")
 				};
 			System.out.println(Arrays.toString(row));
 			}
+			System.out.println("fin requête read");
 		}catch (Exception e) {
 			// TODO: handle exception
 			System.err.println("Erreur d'affichage d'ing: " + e.getMessage());
@@ -50,7 +52,7 @@ public class QueryDragon extends MyConnexion{
 	public static boolean deleteByNamePrepared(String nom) {
 		boolean success=false;
 		try {
-			String query= "DELETE FROM dragons2 where nom = ?";
+			String query= "DELETE FROM dragons2 where Dragon = ?";
 			PreparedStatement declaration=accessDataBase.prepareStatement(query);
 			declaration.setString(1,nom);
 			int executeUpdate=declaration.executeUpdate();
@@ -58,12 +60,13 @@ public class QueryDragon extends MyConnexion{
 	} catch(SQLException e) {
 		System.err.println("Erreur suppression dragons2: "+e.getMessage());
 	}
+		System.out.println("fin requête delete");
 	return success;
 	}
 	public static boolean update(String nom) {
 		boolean success=false;
 		try {
-			String query = "UPDATE dragons2 SET nom=?;";
+			String query = "UPDATE dragons2 SET Dragon=? WHERE Dragon='Denver';";
 			PreparedStatement declaration=accessDataBase.prepareStatement(query);
 			declaration.setString(1,nom);
 			int executeUpdate=declaration.executeUpdate();
@@ -81,9 +84,9 @@ public class QueryDragon extends MyConnexion{
 		//update("poivre");
 		create("Denver",10,"F",250,2300,"non","proche");
 		readAll();
-		update(null);
+		update("DenverA");
 		readAll();
-		deleteByNamePrepared("Denver");
+		deleteByNamePrepared("DenverA");
 		closeConnection();
 		}
 }
